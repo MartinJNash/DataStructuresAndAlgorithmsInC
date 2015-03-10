9961a3e4b5d2e1bcd13b8ad0a9c940202e7f0edb
Martin Nash <martin.j.nash@gmail.com>
2015-03-10 01:47:06 -0700

    Initial Commit

---
+++ PointMeDown/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PointMeDown
+//
+//  Created by Martin Nash on 3/10/15.
+//  Copyright (c) 2015 Martin Nash. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
